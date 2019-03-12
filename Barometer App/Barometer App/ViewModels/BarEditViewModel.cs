7e290c8a4ddc98a98bd3ee2a51b13e6469da8d1e
Simon Wessmann Brok <au586362@uni.au.dk>
2019-03-12 11:44:39 +0100

    Næste step er at få parametre med ved forskellige page pushes

---
+++ Barometer App/Barometer App/ViewModels/BarEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Barometer_App.ViewModels;
+using Prism.Navigation;
+
+namespace Barometer_App.ViewModels
+{
+    class BarEditViewModel : ViewModelBase
+    {
+
+        public BarEditViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
--- Barometer App/Barometer App/ViewModels/MainPageViewModel.cs
+++ Barometer App/Barometer App/ViewModels/MainPageViewModel.cs
@@ -3,32 +3,53 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows.Input;
+using Barometer_App.Models;
 
 namespace Barometer_App.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public ObservableCollection<Bar> Bars;
+
         private readonly INavigationService _navigationService;
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             _navigationService = navigationService;
             Title = "Bar-O-Meter";
+            Bars = new ObservableCollection<Bar>(new List<Bar>(){
+                new Bar()
+                {Name = "Dat Bar", Address = "Peter Bøgh", BarId = 1,
+                    CVR = 21312213, Email = "Dat@bar.dk", Image = "chess.png",
+                    AboutText = "Datbar sutter", Rating = 12
+                },
+                new Bar()
+                {
+                    Name = "Katrines kælder", Address = "Katrionebjerg", BarId = 0,
+                    CVR = 21203050, Email = "KK@kk.dk", Image = "katrine.png",
+                    AboutText = "Katrines kælder er fredagsbar for ingeniørene på katrinebjerg.", Rating = 14
+
+                }
+            });
         }
 
         #region Commands
 
-        private ICommand _showBarsCommand;
+        private ICommand _navShowBarsCommand;
 
-        public ICommand ShowBarsCommand => _showBarsCommand ?? (_showBarsCommand = new DelegateCommand(OnShowBars));
+        public ICommand NavShowBarsCommand => _navShowBarsCommand ?? (_navShowBarsCommand = new DelegateCommand(OnNavShowBars));
 
-        async private void OnShowBars()
+        private async void OnNavShowBars()
         {
-            await _navigationService.NavigateAsync("BarList");
+            //DETTE ER NÆSTE STEP.
+            var parameters = new NavigationParameters {{"Bars", Bars}};
+
+            await _navigationService.NavigateAsync("BarList",parameters);
         }
 
         #endregion
