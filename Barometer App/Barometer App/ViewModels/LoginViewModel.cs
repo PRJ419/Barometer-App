432a4649d8c70982a96569097e6f260a4420435e
Christian Wraa Melsen <christian@melsen.info>
2019-04-04 20:28:53 +0200

    Did some viewmodel updates for Login

--- Barometer App/Barometer App/ViewModels/LoginViewModel.cs
+++ Barometer App/Barometer App/ViewModels/LoginViewModel.cs
@@ -1,13 +1,38 @@
-﻿using Barometer_App.ViewModels;
+﻿using System.Windows.Input;
+using Barometer_App.ViewModels;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace Barometer_App.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        private readonly INavigationService _navigationService;
+
         public LoginViewModel(INavigationService navigationService) : base()
         {
             Title = "Login";
         }
+
+        #region NavCommands
+        
+        private ICommand _navSignupCommand;
+
+        public ICommand NavSignupCommand =>
+            _navSignupCommand ?? (_navSignupCommand = new DelegateCommand(OnNavSignup));
+
+        public async void OnNavSignup()
+        {
+            var navParams = new NavigationParameters();
+            await _navigationService.NavigateAsync("Signup", navParams);
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
     }
 }
