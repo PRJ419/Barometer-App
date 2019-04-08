1e89c062987b30f0df1aecfc002e8e0127b52e94
ALE <holmern251@gmail.com>
2019-04-08 08:14:59 +0200

    De fleste nye funktioner er tilføjet, med undtagelse af et par jeg skal snakke med tobi om. Tilføjelser skal dog testes

--- Barometer App/Barometer App/Data Access/RESTClient.cs
+++ Barometer App/Barometer App/Data Access/RESTClient.cs
@@ -16,7 +16,7 @@ namespace RESTClient
 
         //BAR
 
-        //GET api/bars/
+        //GET /api/bars/
         public async Task<List<BarSimpleDto>> GetBarList()
         {
             using (var client = new HttpClient())
@@ -46,7 +46,7 @@ namespace RESTClient
             }
         }
 
-        //PUT api/bars/
+        //PUT /api/bars/
         public async Task<bool> EditBar(BarDto editedBar) //Virker ikke
         {
             using (var client = new HttpClient())
@@ -71,7 +71,7 @@ namespace RESTClient
             }
         }
 
-        //POST api/bars/
+        //POST /api/bars/
         public async Task<bool> CreateBar(BarDto newBar)
         {
             using (var client = new HttpClient())
@@ -96,7 +96,7 @@ namespace RESTClient
             }
         }
 
-        //GET api/bars/{id}
+        //GET /api/bars/{id}
         public async Task<BarDto> GetDetailedBar(string id)
         {
             using (var client = new HttpClient())
@@ -127,7 +127,7 @@ namespace RESTClient
             }
         }
 
-        //DELETE api/bars/{id}
+        //DELETE /api/bars/{id}
         public async Task<bool> DeleteBar(string id)
         {
             using (var client = new HttpClient())
@@ -147,7 +147,7 @@ namespace RESTClient
             }
         }
 
-        //GET api/bars/worst
+        //GET /api/bars/worst
         public async Task<List<BarSimpleDto>> GetWorstBarList()
         {
             using (var client = new HttpClient())
@@ -177,7 +177,7 @@ namespace RESTClient
             }
         }
 
-        //GET api/bars/{from}/{to}
+        //GET /api/bars/{from}/{to}
         public async Task<List<BarSimpleDto>> GetSpecificBarList(int index1, int index2) //Virker men weird, skal lige snakke med tobi
         {
             using (var client = new HttpClient())
@@ -207,8 +207,11 @@ namespace RESTClient
             }
         }
 
-        //GET api/bars/{barname}/drinks
-        public async Task<List<DrinkDto>> GetBarDrinkList(string id)
+
+        //DRINK
+
+        //GET /api/bars/{barname}/drinks
+        public async Task<List<DrinkDto>> GetBarDrinkList(string id) //Skal testes
         {
             using (var client = new HttpClient())
             {
@@ -225,7 +228,7 @@ namespace RESTClient
                         return drinks;
                     }
 
-                    //GetAsync failed, returning empty list of bars
+                    //GetAsync failed, returning empty list of drinks
                     return new List<DrinkDto>();
                 }
                 catch (Exception e)
@@ -237,6 +240,266 @@ namespace RESTClient
             }
         }
 
-        
+        //PUT /api/bars/{barname}/drinks
+        public async Task<bool> EditDrink(DrinkDto editedDrink, string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var jsonString = JsonConvert.SerializeObject(editedDrink);
+                Console.WriteLine(jsonString); //Test
+
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync($"api/bars/{id}/drinks", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Drink has been updated!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong: {response.StatusCode}"); // Test
+                return false;
+            }
+        }
+
+        //POST /api/bars/{barname}/drinks
+        public async Task<bool> CreateDrink(DrinkDto newDrink, string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var jsonString = JsonConvert.SerializeObject(newDrink);
+
+                Console.WriteLine($"json : \n{jsonString}"); //Test
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync($"api/bars/{id}/drinks", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Drink has been created!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong {response.StatusCode}"); //Test
+                return false;
+            }
+        }
+
+        //DELETE /api/bars/{barname}/drinks HVORDAN FINDER VI DEN SPECIFIKKE DRINK?
+        /*public async Task<bool> DeleteDrink(string barId)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var response = await client.DeleteAsync($"api/bars/{barId}/???????");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Drink has been deleted!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong {response.StatusCode}"); //Test
+                return false;
+            }
+        }*/
+
+
+        //EVENT
+
+        //GET /api/bars/{barname}/events
+        public async Task<List<BarEventDto>> GetBarEventList(string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                try
+                {
+                    var response = await client.GetAsync($"api/bars/{id}/events/");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var msg = await response.Content.ReadAsStringAsync();
+                        var events = JsonConvert.DeserializeObject<List<BarEventDto>>(msg);
+
+                        return events;
+                    }
+
+                    //GetAsync failed, returning empty list of events
+                    return new List<BarEventDto>();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der var ingen events at hente");
+                    //There were no events to get, returning empty list of events
+                    return new List<BarEventDto>();
+                }
+            }
+        }
+
+        //PUT /api/bars/{barname}/events
+        public async Task<bool> EditEvent(BarEventDto editedEvent, string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var jsonString = JsonConvert.SerializeObject(editedEvent);
+                Console.WriteLine(jsonString); //Test
+
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync($"api/bars/{id}/events", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Event has been updated!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong: {response.StatusCode}"); // Test
+                return false;
+            }
+        }
+
+        //POST /api/bars/{barname}/events
+        public async Task<bool> CreateEvent(BarEventDto newEvent, string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var jsonString = JsonConvert.SerializeObject(newEvent);
+
+                Console.WriteLine($"json : \n{jsonString}"); //Test
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync($"api/bars/{id}/events", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Event has been created!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong {response.StatusCode}"); //Test
+                return false;
+            }
+        }
+
+        //DELETE /eventname
+        public async Task<bool> DeleteEvent(string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var response = await client.DeleteAsync($"{id}/");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Event has been deleted!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong {response.StatusCode}"); //Test
+                return false;
+            }
+        }
+
+
+        //REVIEW
+
+        //GET /api/bars/{barname}/reviews
+        public async Task<List<ReviewDto>> GetBarReviewList(string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                try
+                {
+                    var response = await client.GetAsync($"api/bars/{id}/reviews/");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var msg = await response.Content.ReadAsStringAsync();
+                        var events = JsonConvert.DeserializeObject<List<ReviewDto>>(msg);
+
+                        return events;
+                    }
+
+                    //GetAsync failed, returning empty list of reviews
+                    return new List<ReviewDto>();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der var ingen reviews at hente");
+                    //There were no events to get, returning empty list of reviews
+                    return new List<ReviewDto>();
+                }
+            }
+        }
+
+        //PUT /api/bars/{barname}/reviews
+        public async Task<bool> EditReview(ReviewDto editedReview, string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var jsonString = JsonConvert.SerializeObject(editedReview);
+                Console.WriteLine(jsonString); //Test
+
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync($"api/bars/{id}/reviews", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Review has been updated!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong: {response.StatusCode}"); // Test
+                return false;
+            }
+        }
+
+        //POST /api/bars/{barname}/reviews
+        public async Task<bool> CreateReview(ReviewDto newReview, string id) //Skal testes
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseaddress);
+
+                var jsonString = JsonConvert.SerializeObject(newReview);
+
+                Console.WriteLine($"json : \n{jsonString}"); //Test
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync($"api/bars/{id}/reviews", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Review has been created!"); //Test
+                    return true;
+                }
+
+                Console.WriteLine($"Something went wrong {response.StatusCode}"); //Test
+                return false;
+            }
+        }
+
+        //DELETE /api/bars/{barname}/reviews
+        //SKAL VI SLETTE LISTEN AF REVIEWS ELLER SPECIFIK?
+
+        //GET /api/bars/{barname}/reviews/{username}
+        //GETTER VI KUN EN SPECIFIK PERSONS REVIEW FOR EN BESTEMT BAR ELLER FOR ALLE BARER?
     }
 }
