c79814229d2dfdd3e281ee6a6ed4ecfea2579d40
Welle77 <36986182+Welle77@users.noreply.github.com>
2019-04-24 12:56:16 +0200

    Beautification to DetailedBar. DetailedBarEdit done.

--- Barometer App/Barometer App/ViewModels/DetailedBarEditViewModel.cs
+++ Barometer App/Barometer App/ViewModels/DetailedBarEditViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Barometer_App.Models;
+using Prism.Commands;
 using Prism.Navigation;
 
 namespace Barometer_App.ViewModels
@@ -12,17 +14,28 @@ namespace Barometer_App.ViewModels
         {
             get => _bar;
             set => SetProperty(ref _bar, value);
-        }
+        }       
 
-        public DetailedBarEditViewModel()
+        public DetailedBarEditViewModel(INavigationService navigationService)
         {
             Bar = new Bar();
             Title = "Detailed Bar Editing Page";
+            NavigationService = navigationService;
         }
 
         public override async void OnNavigatedTo(INavigationParameters parameters)
         {
             Bar = await RestClient.GetDetailedBar("Barname");
+        }
+
+        private ICommand _saveCommand;
+
+        public ICommand SaveCommand => _saveCommand ?? (_saveCommand = new DelegateCommand(SaveExecute));
+
+        private async void SaveExecute()
+        {
+            await RestClient.EditBar(Bar);
+            await NavigationService.GoBackAsync();
         }
     }
 }
