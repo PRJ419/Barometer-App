3bceb3b0fe95c04f7c93ebba598ae241cfdb0bf3
ALE <holmern251@gmail.com>
2019-05-06 15:07:10 +0200

    I REST client er error messages tilføjet for register user, login

--- Barometer-App/Barometer-App/Data Access/RESTClient.cs
+++ Barometer-App/Barometer-App/Data Access/RESTClient.cs
@@ -124,7 +124,7 @@ namespace RESTClient
 
                     if (response.StatusCode == HttpStatusCode.BadRequest)
                     {
-                        if (response.ReasonPhrase.ToLower().Contains("duplicate"))
+                        if (response.ReasonPhrase.ToLower().Contains("e"))
                             throw new DuplicateNameException("Bar or BarRep already exists");
                     }
 
@@ -1423,10 +1423,16 @@ namespace RESTClient
 
                 if (response.StatusCode == HttpStatusCode.BadRequest)
                 {
-                    if (response.ReasonPhrase.ToLower().Contains("duplicate"))
+                    if (response.ReasonPhrase.ToLower().Contains("DuplicateName"))
                         throw new DuplicateNameException("Username already exists");
+                    //God kode her...........
+                    if(response.ReasonPhrase.ToLower().Contains("PasswordRequireUpper"))
+                        throw new Exception("Password must contain at least one uppercase letter, one lowercase letter and a number");
 
-                    if(response.ReasonPhrase.ToLower().Contains("password"))
+                    if (response.ReasonPhrase.ToLower().Contains("PasswordRequireLower"))
+                        throw new Exception("Password must contain at least one uppercase letter, one lowercase letter and a number");
+
+                    if (response.ReasonPhrase.ToLower().Contains("PasswordRequireDigit"))
                         throw new Exception("Password must contain at least one uppercase letter, one lowercase letter and a number");
                 }
 
@@ -1461,7 +1467,7 @@ namespace RESTClient
 
                 if (response.StatusCode == HttpStatusCode.BadRequest)
                 {
-                    if(response.ReasonPhrase.ToLower().Contains("password"))
+                    if(response.ReasonPhrase.ToLower().Contains("Invalid login"))
                         throw new Exception("Wrong username and/or password");
                 }
 
